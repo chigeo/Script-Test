a02e0e2fab4e72109be3b64a9108d9ab83750b49
chigeo <misasagi.119@gmail.com>
2016-09-20 14:10:36 +0900

    Script Test　再提出

---
+++ Assets/Test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int[] array = {13,32,8,16,34} ;
+		for(int c = array.Length -1 ; 0 <= c; c--){
+			Debug.Log(array[c]);
+		}
+
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
